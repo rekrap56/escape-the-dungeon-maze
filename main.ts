6b4dd8db524774ec6ccf65416938bbfcb649a04f
rekrap56 <66746434+rekrap56@users.noreply.github.com>
2020-06-12 15:55:22 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,178 @@
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+    `
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+        2 2 2 2 2 2 2 2 2 2 2 2 2 4 4 2
+        2 4 2 2 2 2 4 2 4 4 4 2 2 2 4 4
+        2 2 2 2 2 2 4 4 2 2 2 2 4 2 2 4
+        2 2 5 5 2 2 2 2 2 2 5 5 4 2 2 4
+        2 2 4 2 2 2 2 2 2 2 4 2 2 2 2 2
+        2 2 2 2 2 2 5 2 2 4 4 2 2 4 2 2
+        2 2 2 2 4 4 2 2 2 2 2 2 2 4 2 2
+        2 2 4 4 5 5 2 2 4 2 2 2 5 2 2 2
+        2 2 4 5 2 2 2 2 4 2 2 2 2 2 2 2
+        2 2 2 2 2 4 4 2 4 2 2 2 4 4 2 2
+        2 2 2 2 2 2 2 4 2 4 4 4 4 4 2 2
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+        2 4 4 2 2 2 2 2 2 2 4 2 5 2 2 2
+        2 4 4 2 4 2 2 2 2 4 4 4 5 2 2 2
+        2 2 2 2 2 4 4 2 2 2 2 4 2 2 2 2
+        2 2 5 2 2 2 2 2 2 2 4 4 2 2 2 2
+    `
+    //% blockIdentity=images._tile
+    export const tile2 = img`
+        d f d d d d d d d d d d d d d d
+        d d d d d d f f d d d d f d d d
+        d d d d f f d d f d d d f d d d
+        d d f f d d d d f d d d d f d d
+        d d d d d d d d d f d d d d f d
+        d d f f d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d f f f f f d d d d
+        d d d f f f f d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d f f f f d
+        d d d d d d f f d d d d d d d d
+        d d d d d d d d f f d d d d d d
+        d d f f f f d d d d d d d d f d
+        d d d d d d f f f f f d d f d d
+        d d d d d d d d d d d d d d d d
+    `
+    //% blockIdentity=images._tile
+    export const tile3 = img`
+        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+        7 7 f f f f f 7 7 f f f f 7 7 7
+        7 7 f 7 7 7 f 7 7 f 7 7 f f f 7
+        7 7 f 7 7 7 f 7 7 f 7 7 7 7 f 7
+        7 f 7 7 7 7 7 7 7 f 7 7 7 f f 7
+        7 f 7 f f f 7 7 f f 7 7 7 f 7 7
+        7 f f 7 7 f f 7 f 7 7 7 7 f 7 7
+        7 7 f f 7 7 f 7 f 7 7 7 f f 7 7
+        7 7 7 f f f f 7 f f f 7 f 7 7 7
+        7 7 7 7 7 7 7 7 7 7 7 f f 7 7 7
+        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    `
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+    `
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    music.baDing.play()
+    ree.destroy()
+    info.changeScoreBy(1)
+})
+function setup () {
+    tiles.setTilemap(tiles.createTilemap(
+            hex`100010000202020202020202020202020202020202020203030303030303030303030303020202020c0a020202020202020a0a07020a0a020a0a0a0a0a0a0a0a020a0a01020a0a02020202020202020a020a0a02020a0a0a0a0a0a0a0a02020a020a0a02020a020a0202020202020a0a0a0a0202020a02020a0a0a0a020a0a0a010a0a02020a020a0202020a020a010102010a02020a0a0a0202020a020a020a02020a02020a0a0a0a0a0a0a0a0a020a02020a02020a020202020a02020a0a0a02020102020a020a020a0a0a02010101010202020b0a020a0a0a020a02020202020202020202020101010202020202020202020202020202020202020202020202020202`,
+            img`
+                2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+                2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+                2 2 2 2 . . 2 2 2 2 2 2 2 . . .
+                2 . . 2 . . . . . . . . 2 . . 2
+                2 . . 2 2 2 2 2 2 2 2 . 2 . . 2
+                2 . . . . . . . . 2 2 . 2 . . 2
+                2 . 2 . 2 2 2 2 2 2 . . . . 2 2
+                2 . 2 2 . . . . 2 . . . 2 . . 2
+                2 . 2 . 2 2 2 . 2 . 2 2 2 2 . 2
+                2 . . . 2 2 2 . 2 . 2 . 2 2 . 2
+                2 . . . . . . . . . 2 . 2 2 . 2
+                2 . 2 2 2 2 . 2 2 . . . 2 2 2 2
+                2 . 2 . 2 . . . 2 2 2 2 2 2 2 2
+                . . 2 . . . 2 . 2 2 2 2 2 2 2 2
+                2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+                2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+            `,
+            [myTiles.tile0,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundCenter,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundEast,sprites.dungeon.collectibleBlueCrystal,sprites.dungeon.collectibleRedCrystal,sprites.dungeon.collectibleInsignia,sprites.dungeon.chestClosed,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4],
+            TileScale.Sixteen
+        ))
+    player = sprites.create(img`
+        . . . 8 8 8 8 8 8 8 . . . . . .
+        . . . . 8 8 8 8 8 8 . . . . . .
+        . . . . 8 8 8 8 8 8 . . . . . .
+        . . . . . 8 8 8 8 8 . . . . . .
+        . . . . . . 8 8 8 8 . . . . . .
+        . . . . . 8 8 8 . . 8 8 8 8 . .
+        . . . . 8 8 . 8 . . . . . 8 8 .
+        . . . . 8 . . 8 . . . . . . . .
+        . . . . . . . 8 . . . . . . . .
+        . . . . . . 8 8 8 . . . . . . .
+        . . . . . . 8 8 . 8 8 . . . . .
+        . . . . . 8 8 . . . 8 . . . . .
+        . . . . . 8 . . . . . 8 . . . .
+        . . . . 8 8 . . . . . 8 8 . . .
+        . . . 8 8 . . . . . . . 8 8 . .
+        . . . . . . . . . . . . . . . .
+    `, SpriteKind.Player)
+    ree = sprites.create(img`
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . 5 5 . . . . . .
+        . . . . . . 5 5 5 5 5 5 . . . .
+        . . . . . 5 5 5 5 5 5 5 5 . . .
+        . . . . . 5 5 5 5 5 5 5 5 . . .
+        . . . . . 5 5 5 5 5 5 5 5 . . .
+        . . . . 5 5 5 5 5 5 5 5 5 . . .
+        . . . . . 5 5 5 5 5 5 5 5 . . .
+        . . . . . 5 5 5 5 5 5 5 5 . . .
+        . . . . . . 5 5 5 5 5 5 . . . .
+        . . . . . . 5 5 5 5 5 . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+    `, SpriteKind.Food)
+    info.startCountdown(20)
+    info.setScore(0)
+    controller.moveSprite(player)
+    scene.cameraFollowSprite(player)
+    tiles.placeOnRandomTile(player, myTiles.tile3)
+    tiles.placeOnRandomTile(ree, myTiles.tile2)
+}
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleInsignia, function (sprite, location) {
+    if (info.score() > 0) {
+        game.over(true)
+    }
+})
+
+let player: Sprite = null
+let ree: Sprite = null
+setup()
